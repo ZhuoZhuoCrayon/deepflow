779dcfff6337982919a4c9804a668b5ea0272ddf
pegasusljn <pegasusljn@gmail.com>
2025-02-24 20:31:14 +0800

    fix: refine condition logic for exportFieldCategoryBits in the IsExportField # Reviewed, transaction id: 31289

--- server/ingester/exporters/exporters.go
+++ server/ingester/exporters/exporters.go
@@ -171,12 +171,17 @@ func IsExportField(tag *config.StructTags, exportFieldCategoryBits uint64, expor
 		return false
 	}
 
+	// for category_k8s_label
+	if config.K8S_LABEL&exportFieldCategoryBits != 0 {
+		return true
+	}
+	// for category_tag, category_metrics
 	if tag.CategoryBit&exportFieldCategoryBits != 0 && tag.SubCategoryBit&exportFieldCategoryBits != 0 {
 		return true
 	}
 
 	for _, name := range exportFieldNames {
-		if name == tag.Name {
+		if name == tag.Name || name == tag.MapName {
 			return true
 		}
 	}
